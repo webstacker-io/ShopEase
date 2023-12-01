fd589150476fa5569a7253ad36b3ed9978c29c30
Karthick <webstacker.io@gmail.com>
2023-12-01 19:59:51 +0530

    Mock updated and Read me file updated

--- README.md
+++ README.md
@@ -22,6 +22,17 @@ Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.
 
 Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
 
+## Assumptions
+
+1. Assumed that the grid will be having 4 columns of data.
+2. Placed only the plaecholder image for all the cards because of time constraint.
+
+## Challenges
+
+1. Design & responsiveness on the pure HTML & css
+2. Animations on hover of the product card implementation using Angular Animmations
+3. Implementation of the overlay to display the description of a product.
+
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
--- src/app/shared/mocks/products.ts
+++ src/app/shared/mocks/products.ts
@@ -1,59 +1,58 @@
 export const products = [
   {
-    name: 'Product 1',
+    name: 'The Alchemist',
     imageUrl: 'https://picsum.photos/seed/picsum/200/300',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    description: 'Embark on a journey with Santiago, a shepherd, as he follows his dream across the desert in search of a hidden treasure. A timeless tale of self-discovery, destiny, and the pursuit of a personal legend.',
     price: '$19.99',
     category: 'Books'
   },
   {
-    name: 'Product 2',
+    name: 'Brave New World',
     imageUrl: 'https://picsum.photos/seed/picsum/200/300',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    description: 'In a dystopian future, Huxley envisions a society where pleasure and stability are prized above all else, exploring the consequences of sacrificing individuality and free will. A thought-provoking classic that challenges the norms of societal perfection.',
     price: '$29.99',
     category: 'Books'
   },
   {
-    name: 'Product 1',
+    name: 'To Kill a Mockingbird',
     imageUrl: 'https://picsum.photos/seed/picsum/200/300',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    description: 'Set in the racially charged American South, young Scout Finch recounts her courageous defense of a black man falsely accused of raping a white woman. A poignant exploration of morality, justice, and compassion in the face of prejudice.',
     price: '$19.99',
     category: 'Books'
   },
   {
-    name: 'Product 2',
+    name: 'Sapiens',
     imageUrl: 'https://picsum.photos/seed/picsum/200/300',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    description: 'Harari takes readers on an enlightening journey through the history of Homo sapiens, from the ancient foraging societies to the modern interconnected world. An insightful exploration of the forces that shaped human societies and the impact of our shared history.',
     price: '$29.99',
     category: 'Books'
   },
   {
-    name: 'Product 1',
+    name: 'Stride Bliss',
     imageUrl: 'https://picsum.photos/seed/picsum/200/300',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    description: 'Experience unparalleled comfort and support with Stride Bliss, designed for the modern adventurer. With advanced cushioning technology and a sleek design, these shoes effortlessly blend style and functionality.',
     price: '$19.99',
     category: 'Shoes'
   },
   {
-    name: 'Product 2',
+    name: 'Urban Sole',
     imageUrl: 'https://picsum.photos/seed/picsum/200/300',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    description: 'Urban Sole merges urban fashion with exceptional comfort, offering a stylish and versatile footwear choice for city dwellers. These shoes seamlessly combine contemporary design with durability for an everyday fashion statement.',
     price: '$29.99',
     category: 'Shoes'
   },
   {
-    name: 'Product 1',
+    name: 'SportFlex',
     imageUrl: 'https://picsum.photos/seed/picsum/200/300',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    description: 'Elevate your athletic performance with SportFlex, featuring dynamic flexibility and responsive cushioning. Whether you are hitting the gym or pounding the pavement, these shoes provide the perfect balance of comfort and agility.',
     price: '$19.99',
     category: 'Shoes'
   },
   {
-    name: 'Product 2',
+    name: 'SkyWalk',
     imageUrl: 'https://picsum.photos/seed/picsum/200/300',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    description: 'Step into the future of footwear with SkyWalk, where innovation meets style in every stride. These shoes boast cutting-edge design and lightweight construction, making them the ideal choice for those who appreciate both fashion and functionality.',
     price: '$29.99',
     category: 'Shoes'
-  },
-  // Add more products as needed
+  }
 ];
